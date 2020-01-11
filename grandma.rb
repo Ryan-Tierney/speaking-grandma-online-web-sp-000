27de9c76e882f487f1f3d6c33158cf7ea0e5b08e
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-01-11 03:30:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,12 +16,9 @@ def speak_to_grandma(phrase)
   if 
      "#{phrase.upcase}" == "I LOVE YOU GRANDMA!"
    return "I LOVE YOU TOO PUMPKIN!"
- "#{phrase.upcase}" == "#{phrase}"
-  return "NO, NOT SINCE 1938!"
-
 elsif 
- "#{phrase.upcase}" == "I LOVE YOU GRANDMA!"
-   return "I LOVE YOU TOO PUMPKIN!"
+  "#{phrase.upcase}" == "#{phrase}"
+  return "NO, NOT SINCE 1938!"
 else
    return "HUH?! SPEAK UP, SONNY!"
 end
