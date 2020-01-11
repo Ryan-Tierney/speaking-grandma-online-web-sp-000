73178cd0b6953311f28e34dc19908ae47023a513
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-01-11 03:30:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,8 @@
 
 def speak_to_grandma(phrase)
   if 
-    
+     "#{phrase.upcase}" == "I LOVE YOU GRANDMA!"
+   return "I LOVE YOU TOO PUMPKIN!"
  "#{phrase.upcase}" == "#{phrase}"
   return "NO, NOT SINCE 1938!"
 
